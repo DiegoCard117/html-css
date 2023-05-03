8397c390263153004dd307de334fdf3aa02d22ea
DiegoCard117 <diegocardoso117@gmail.com>
2023-05-03 20:38:14 -0300

    Initial commit

---
+++ exercicios/ex021/scrypt.js
@@ -0,0 +1,15 @@
+function faceacao(){
+    window.open ("https://www.facebook.com/profile.php?id=100033128117354", "_blank")
+}
+
+function gitacao(){
+    window.open ("https://github.com/DiegoCard117?tab=repositories", "_blank")
+}
+
+function instacao(){
+    window.open ("https://www.instagram.com/dih_cardoso117/", "_blank")
+}
+
+function youtubeacao(){
+    window.open ("https://www.youtube.com/channel/UCKpZkh05QHZ4Whq2CutY6JQ", "_blank")
+}
